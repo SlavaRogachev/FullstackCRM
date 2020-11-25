e23e37912265af575f6a27367e089840bdf38303
Вячеслав Рогачёв <vr@tortu.agency>
2020-11-25 14:01:33 +0200

    Добавлена авторизация и регистрация пользователей, взаимодействие с MongoDB

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+// const cors = require('cors')  
+// const morgan = require('morgan')
+const mongoose = require('mongoose')
 const authRoutes = require('./routes/auth')
 const analiticsRoutes = require('./routes/analitics')
 const categoryRoutes = require('./routes/category')
 const orderRoutes = require('./routes/order')
 const positionRoutes = require('./routes/position')
+const keys = require('./config/keys')
 const app = express()
 
+mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+})
+    .then(() => console.log('MongoDB connected'))
+    .catch(e => console.log(e))
+
+app.use(require('morgan')('dev'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+app.use(require('cors')())
 
 app.use('/api/auth', authRoutes)
 app.use('/api/analitics', analiticsRoutes)
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,59 @@
-module.exports.login = function(req, res) {
-    res.status(200).json({
-        // login: {
-        //     email: req.body.email,
-        //     password: req.bode.password
-        // }
-        login: req.body
-    })
+
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+const keys = require('../config/keys')
+
+
+
+
+module.exports.login = async function(req, res) {
+    const candidate = await User.findOne({email: req.body.email})
+
+    if (candidate) {
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password)
+        if (passwordResult) {
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id
+            }, keys.jwt, {expiresIn: 60 * 60})
+
+            res.status(200).json({
+                token: `Bearer ${token}`
+            })
+        } else {
+            res.status(401).json({
+                message: 'Неверный пароль'
+            })
+        }
+    } else {
+        res.status(404).json({
+            message: 'Пользователя с таким email не существует'
+        })
+    }
 }
 
-module.exports.register = function(req, res) {
-    res.status(200).json({
-        register: 'from controller'
-    })
+module.exports.register = async function(req, res) {
+    const candidate = await User.findOne({email: req.body.email})
+
+    if (candidate) {
+        res.status(409).json({
+            message: 'Пользователь с таким email уже существует'
+        })
+    } else {
+
+        const salt = bcrypt.genSaltSync(10)
+        const password = req.body.password
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt)
+        })
+        try {
+            await user.save()
+            res.status(201).json(user)
+        } catch(e) {
+            //
+        }
+        
+    }
 }
